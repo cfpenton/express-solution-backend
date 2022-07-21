6c96fdc1cd113bb4788a1d3caa12b7ababe4b35a
cfpenton <cfpenton@gmx.com>
2022-07-21 12:06:58 -0400

    configdb-renow

--- dbConfig.js
+++ dbConfig.js
@@ -1,20 +1,12 @@
 const mysql = require('mysql2');
 
 //Environment variable
-/* const mysqlConnection = mysql.createConnection({
+const mysqlConnection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: 'root',
     database: 'caol'
-}); */
-
-const mysqlConnection = mysql.createConnection({
-    host: 'sql3.freesqldatabase.com',
-    user: 'sql3507753',
-    password: 'CAzTGwhT5Q',
-    database: 'sql3507753',
-    port: 3306
-}); 
+});
 
 mysqlConnection.connect(function (err) {
     if (err) {
