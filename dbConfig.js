8f15d1f327b3d6291d9a2f5001e7b3829e8adf9a
cfpenton <cfpenton@gmx.com>
2022-07-25 11:50:11 -0400

    test-db

--- dbConfig.js
+++ dbConfig.js
@@ -9,20 +9,20 @@ const mysql = require('mysql2');
     database: 'bjy65fmikhjvqmzmt2sh'
 }); */
 /* servidor-win-remoto */
-const mysqlConnection = mysql.createConnection({
+/* const mysqlConnection = mysql.createConnection({
     host: 'buhigz693lipvlr9tnns-mysql.services.clever-cloud.com',
     user: 'u4vme26uukafdfdg',
     password: 'cwy9EYCGlJURuW9oobUe',
     database: 'buhigz693lipvlr9tnns'
-}); 
+});  */
 
 /* servidor-linux-remoto */
-/* const mysqlConnection = mysql.createConnection({
+const mysqlConnection = mysql.createConnection({
     host: 'sql3.freesqldatabase.com',
     user: 'sql3507753',
     password: 'CAzTGwhT5Q',
     database: 'sql3507753'
-}); */
+});
 
 /* servidor-local */
 /* const mysqlConnection = mysql.createConnection({
